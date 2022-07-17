c865f8ce026d1de8cb654570da622ab27fba7547
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 23:46:19 -0700

    added the comments for the better grip

--- src/LevelOrderTreeTraversal.java
+++ src/LevelOrderTreeTraversal.java
@@ -3,6 +3,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+
+// tree node
 class TreeNode{
     int val;
     TreeNode left;
@@ -15,27 +17,38 @@ class TreeNode{
 }
 
 public class LevelOrderTreeTraversal {
+    // method to return list of lists(containing the tree nodes level wise)
     public static List<List<Integer>> levelOrderTraversal(TreeNode root){
+
+        // get a list of list
         List<List<Integer>> ans = new LinkedList<>();
         if(root == null) return ans;
-
+        //queue will add the nodes first
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
 
+        //until the queue is empty
         while(!q.isEmpty()){
+            // get the current size of queue and traverse
             int size = q.size();
+            // get a list to add the values of the nodes in it
             ArrayList<Integer> list = new ArrayList<>();
-
+                // until size of queue becomes 0
             while(size>0){
+                // poll the element from queue and then add its value in list and decrease the size
                 TreeNode temp = q.poll();
                 list.add(temp.val);
                 size --;
+                // but if it has right or left child as well then add them in the queue as well
+                // after this return back to the line number 31 and repeat if there is more
                 if(root.left != null) q.add(root.left);
                 if(root.right != null) q.add(root.right);
             }
+            // also dont forget to add it in the list of lists
             ans.add(list);
 
-        }return ans;
+        }// at the end simply return it thanks
+        return ans;
 
     }
 
