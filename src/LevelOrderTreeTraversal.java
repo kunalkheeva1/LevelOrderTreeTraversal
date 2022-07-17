f143ca547ee0ff30bf3c33efb5a379e3be19e3b5
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 23:38:18 -0700

    created a method to return the list of list which contains the nodes as per the levels of the tree;

--- src/LevelOrderTreeTraversal.java
+++ src/LevelOrderTreeTraversal.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class TreeNode{
     int val;
@@ -11,13 +14,32 @@ class TreeNode{
     }
 }
 
-
-
-
-
-
 public class LevelOrderTreeTraversal {
-    public static List<List<Integer>> levelOrderTraversal()
+    public static List<List<Integer>> levelOrderTraversal(TreeNode root){
+        List<List<Integer>> ans = new LinkedList<>();
+        if(root == null) return ans;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+            ArrayList<Integer> list = new ArrayList<>();
+
+            while(size>0){
+                TreeNode temp = q.poll();
+                list.add(temp.val);
+                size --;
+                if(root.left != null) q.add(root.left);
+                if(root.right != null) q.add(root.right);
+            }
+            ans.add(list);
+
+        }return ans;
+
+    }
+
+
     public static void main(String[] args) {
 
     }
