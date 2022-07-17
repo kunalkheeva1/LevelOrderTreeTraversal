9616a964033c0559976137c5200840bc000ed8bd
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 23:30:20 -0700

    created a treenode class

--- src/LevelOrderTreeTraversal.java
+++ src/LevelOrderTreeTraversal.java
@@ -1,5 +1,24 @@
+import java.util.List;
+
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val, TreeNode left, TreeNode right){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+
+
+
+
 public class LevelOrderTreeTraversal {
+    public static List<List<Integer>> levelOrderTraversal()
     public static void main(String[] args) {
-        
+
     }
 }
